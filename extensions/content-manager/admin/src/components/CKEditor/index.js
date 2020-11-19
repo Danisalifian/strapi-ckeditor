41de8a847e63ce07d9d9a45483537275608c602e
Danis Alifian <danisalifian@gmail.com>
2020-11-19 08:53:56 +0700

    fix upload image doesnt work in rich text editor

--- extensions/content-manager/admin/src/components/CKEditor/index.js
+++ extensions/content-manager/admin/src/components/CKEditor/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import ClassicEditor from "ckeditor-build-with-simple-upload-provider-strapi";
 import styled from "styled-components";
+import { auth } from "strapi-helper-plugin";
 
 const Wrapper = styled.div`
   .ck-editor__main {
@@ -14,6 +15,8 @@ const Wrapper = styled.div`
 `;
 
 const Editor = ({ onChange, name, value }) => {
+  const jwtToken = auth.getToken();
+
   return (
     <Wrapper>
       <CKEditor
@@ -23,6 +26,14 @@ const Editor = ({ onChange, name, value }) => {
           const data = editor.getData();
           onChange({ target: { name, value: data } });
         }}
+        config={{
+          simpleUpload: {
+            uploadUrl: `${strapi.backendURL}/upload`,
+            headers: {
+              Authorization: "Bearer " + jwtToken,
+            },
+          },
+        }}
       />
     </Wrapper>
   );
@@ -31,7 +42,7 @@ const Editor = ({ onChange, name, value }) => {
 Editor.propTypes = {
   onChange: PropTypes.func.isRequired,
   name: PropTypes.string.isRequired,
-  value: PropTypes.string,
+  value: PropTypes.string.isRequired,
 };
 
 export default Editor;
